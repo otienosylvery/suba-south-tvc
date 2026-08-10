ad2b73dceb86ae5a6c3433acf5f5a1b9d3977e0b
Otieno <sylveryos.os@gmail.com>
2026-08-10 20:45:57 +0300

    tweak

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,7 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/suba-south-tvc/',
-// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/suba-south-tvc/',
 });
